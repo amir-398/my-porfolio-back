b4f321c5e49e4a2655f69dae38bfc45dd9586419
amir-398 <amir.398@hotmail.fr>
2024-04-13 18:05:23 +0200

    Mettre à jour les routes dans projectRoute.ts

--- src/routes/projectRoute.ts
+++ src/routes/projectRoute.ts
@@ -2,15 +2,11 @@ const express = require("express");
 const router = express.Router();
 const projectController = require("../controllers/projectController");
 const jwtMiddleware = require("../middlewares/JwtMiddleware");
-router
-  .route("/")
-  .get(jwtMiddleware.verifyToken, projectController.getAllProjects);
+router.route("/").get(projectController.getAllProjects);
 router
   .route("/addProject")
   .post(jwtMiddleware.verifyToken, projectController.projectRegister);
-router
-  .route("/:id")
-  .get(jwtMiddleware.verifyToken, projectController.getProjectById);
+router.route("/:id").get(projectController.getProjectById);
 router
   .route("/:id")
   .delete(jwtMiddleware.verifyToken, projectController.deleteProjectById);
